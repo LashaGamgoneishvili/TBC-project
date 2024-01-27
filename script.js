8cad2dee83955c7b9233d581f7a9d2bd77c1ad99
lashagamgoneishvili <gamgoneishvili8@gamil.com>
2024-01-27 13:25:37 +0400

    mobile slider

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 //// Scroll to the top of the page before unloading/reloading ///
 
-// window.addEventListener("beforeunload", function () {
-//   window.scrollTo(0, 0);
-// });
+window.addEventListener("beforeunload", function () {
+  window.scrollTo(0, 0);
+});
 
 //////// Sticky Navigation ///////////
 
@@ -50,83 +50,168 @@ menu.addEventListener("click", openMenu);
 
 ////////////   Slider Component    /////////////
 
-// Slider;
-const slider = function () {
-  const slides = document.querySelectorAll(".slide");
-  const btnLeft = document.querySelector(".slider__btn--left");
-  const btnRight = document.querySelector(".slider__btn--right");
-  const dotContainer = document.querySelector(".dots");
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+const dotContainer = document.querySelector(".dots");
 
-  let curSlide = 0;
-  const maxSlide = slides.length;
+let curSlide = 0;
+const maxSlide = slides.length;
 
-  // Functions
-  const createDots = function () {
-    slides.forEach(function (_, i) {
-      dotContainer.insertAdjacentHTML(
-        "beforeend",
-        `<button class="dots__dot" data-slide="${i}"></button>`
-      );
-    });
-  };
-  createDots();
-
-  function goToSlide(slide) {
-    slides.forEach((s) => s.classList.add("hide"));
-    slides[slide].classList.remove("hide");
-  }
-
-  // next slide
-  const nextSlide = function () {
-    if (curSlide === maxSlide - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-    goToSlide(curSlide);
-  };
-
-  // Previous Slide
-  const previous = function () {
-    if (curSlide === 0) {
-      curSlide = maxSlide - 1;
-    } else {
-      curSlide--;
-    }
-    goToSlide(curSlide);
-  };
-
-  setInterval(nextSlide, 4000);
-
-  // Event Handlers
-  btnRight.addEventListener("click", nextSlide);
-  btnLeft.addEventListener("click", previous);
-
-  document.addEventListener("keydown", function (e, curSlide) {
-    console.log(e);
-    if (e.key === "ArrowLeft") previous(curSlide);
-    if (e.key === "ArrowRight") nextSlide(curSlide);
-  });
-
-  dotContainer.addEventListener("click", function (e) {
-    if (e.target.classList.contains("dots__dot")) {
-      const { slide } = e.target.dataset;
-      goToSlide(Number(slide));
-    }
+// Functions
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
   });
 };
+createDots();
 
-slider();
+function goToSlide(slide) {
+  slides.forEach((s) => s.classList.add("hide"));
+  slides[slide].classList.remove("hide");
+}
+
+// next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+// Previous Slide
+const previous = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+// Get the width and height of the viewport
+const screenWidth =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+
+if (screenWidth > 700) {
+  setInterval(nextSlide, 4000);
+}
+
+// Event Handlers
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", previous);
+
+document.addEventListener("keydown", function (e, curSlide) {
+  console.log(e);
+  if (e.key === "ArrowLeft") previous(curSlide);
+  if (e.key === "ArrowRight") nextSlide(curSlide);
+});
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const { slide } = e.target.dataset;
+    if (screenWidth > 700) {
+      goToSlide(Number(slide));
+    } else {
+      slideChanging(slide);
+    }
+  }
+});
+
+////////// style form mobile size /////////
+const slider = document.querySelector(".slider");
+
+const slideChanging = function (slide) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    s.style.opacity = "1";
+  });
+  // Add blur class to the slider for 1 second
+  slides[slide].classList.add("blur");
+  setTimeout(() => {
+    // Remove blur class after 1 second
+    slides[slide].classList.remove("blur");
+  }, 700);
+};
+slideChanging(0);
+
+// next slide
+const nextSlideM = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  slideChanging(curSlide);
+};
+
+// Previous Slide
+const previousM = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  slideChanging(curSlide);
+};
+
+let intervalId;
+
+const startInterval = function () {
+  intervalId = setInterval(nextSlideM, 4000);
+};
+
+if (screenWidth < 700) {
+  startInterval();
+}
+
+let touchStartX, touchEndX;
+
+const mobileSlider = function () {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].addEventListener("touchstart", (e) => {
+      touchStartX = e.touches[0].pageX;
+      clearInterval(intervalId);
+    });
+
+    slides[i].addEventListener("touchmove", (e) => {
+      touchEndX = e.touches[0].pageX;
+      clearInterval(intervalId);
+    });
+
+    slides[i].addEventListener("touchend", () => {
+      if (touchStartX !== undefined && touchEndX !== undefined) {
+        if (touchStartX > touchEndX) {
+          nextSlideM(i);
+        } else {
+          previousM(i);
+        }
+        startInterval();
+        // Reset touch coordinates
+        touchStartX = touchEndX = undefined;
+      }
+    });
+  }
+};
+
+mobileSlider();
+
+for (let i = 0; i < slides.length; i++) {
+  slides[i].addEventListener("touchmove", mobileSlider);
+}
 
 /////////  question section - accordion //////////
 
 const questions = document.querySelectorAll(".question");
 const questionHead = document.querySelectorAll(".question-head");
 
-const questionArr = Array.from(questions);
-
-console.log(questions);
-console.log(questionHead);
 const accordion = function () {
   for (let i = 0; i < questionHead.length; i++) {
     questionHead[i].addEventListener("click", function () {
